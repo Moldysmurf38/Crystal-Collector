de5712ac1c8b8a54d49beada96e798a1198cbd6d
David <davwil38@gmail.com>
2019-08-22 19:10:23 -0400

    cleaned up code

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -9,32 +9,31 @@ var orangeCrystal = $("orange_crystal.png");
 var pinkCrystal = $("pink_crystal.png");
 var purpleCrystal = $("purple_crystal.png");
 
+// Variables for number generator
 var min = 19;
 var max = 120;
-var targetNumber = getRandomInt(min, max)
+
+// Execution of first number generator
+var targetNumber = getRandomInt(min, max);
 $("#target-number").html(targetNumber);
 
+// Function for number generator
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Crystal variables
-var crystalNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-var crystalChoiceBlue = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
-var crystalChoiceOrange = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
-var crystalChoicePink = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
-var crystalChoicePurple = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
-
-console.log(crystalChoiceBlue)
-console.log(crystalChoiceOrange)
-console.log(crystalChoicePink)
-console.log(crystalChoicePurple)
+// Crystal number variables
+var crystalNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+var crystalChoiceBlue = crystalCreation(crystalNumber);
+var crystalChoiceOrange = crystalCreation(crystalNumber);
+var crystalChoicePink = crystalCreation(crystalNumber);
+var crystalChoicePurple = crystalCreation(crystalNumber);
 
 // Crystal Array
 $("img").on("click", function () {
-    var myClass = $(this).attr("id")
+    var myClass = $(this).attr("id");
     if (myClass === "blue-crystal") {
         var blueCrystal = crystalChoiceBlue;
         blueCrystal = parseInt(blueCrystal);
@@ -74,24 +73,25 @@ $("img").on("click", function () {
         targetNumber = getRandomInt(min, max);
         console.log(targetNumber);
         $("#target-number").html(targetNumber);
-    }
-})
+    };
+});
 
-//reset values
+//Reset crystal values
 function reset() {
     scoreTotal = 0;
     $("#score-number").html(scoreTotal);
     crystalNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
-    crystalChoiceBlue = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    crystalChoiceBlue = crystalCreation(crystalNumber);
     $("blue_crystal.png").html(crystalChoiceBlue);
-    crystalChoiceOrange = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    crystalChoiceOrange = crystalCreation(crystalNumber);
     $("orange_crystal.png").html(crystalChoiceOrange);
-    crystalChoicePink = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    crystalChoicePink = crystalCreation(crystalNumber);
     $("pink_crystal.png").html(crystalChoicePink);
-    crystalChoicePurple = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+    crystalChoicePurple = crystalCreation(crystalNumber);
     $("purple_crystal.png").html(crystalChoicePurple);
-    console.log(crystalChoiceBlue);
-    console.log(crystalChoiceOrange);
-    console.log(crystalChoicePink);
-    console.log(crystalChoicePurple);
-}
+};
+
+// Crystal value generating function
+function crystalCreation(crystalNumber) {
+    return crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
+};
